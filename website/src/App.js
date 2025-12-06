d6b52e69f72ab741074671246702ebf7aa5a0934
najsefoster1 <156493859+najsefoster1@users.noreply.github.com>
2025-12-05 20:23:58 -0800

    Replace RecipientPage and CheckoutPage with enhanced versions

--- website/src/App.js
+++ website/src/App.js
@@ -4,6 +4,8 @@ import HomePage from './components/HomePage';
 import UploadPage from './components/UploadPage';
 import RecipientPage from './components/RecipientPage';
 import ReviewPage from './components/ReviewPage';
+import EnhancedRecipientPage from './components/EnhancedRecipientPage';
+import EnhancedCheckoutPage from './components/EnhancedCheckoutPage';
 import CheckoutPage from './components/CheckoutPage';
 import TrackingPage from './components/TrackingPage';
 
@@ -17,9 +19,9 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/upload" element={<UploadPage />} />
-        <Route path="/recipient" element={<RecipientPage />} />
+        <Route path="/recipient" element={<EnhancedRecipientPage />} />
         <Route path="/review" element={<ReviewPage />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/checkout" element={<EnhancedCheckoutPage />} />
         <Route path="/track/:id" element={<TrackingPage />} />
       </Routes>
     </Router>
